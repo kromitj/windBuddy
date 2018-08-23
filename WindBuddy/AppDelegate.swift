de116cf6c3119688c2c9d0a169784cadf247ba1a
Mitch Kroska <kromitj@gmail.com>
2018-08-23 17:47:32 -0500

    Initial Commit

---
+++ WindBuddy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WindBuddy
+//
+//  Created by Mitch Kroska on 8/23/18.
+//  Copyright © 2018 Mitch Kroska. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WindBuddy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindBuddy
+//
+//  Created by Mitch Kroska on 8/23/18.
+//  Copyright © 2018 Mitch Kroska. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
